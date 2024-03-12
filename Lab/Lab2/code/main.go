a01e0b2ee4ecd1accbbfa0252075ea5f81540e2d
Ashside <maybeyushu@outlook.com>
2024-03-12 22:09:25 +0800

    Lab2 随机选择完成

--- Lab/Lab2/code/main.go
+++ Lab/Lab2/code/main.go
@@ -20,7 +20,7 @@ func main() {
 	for i := range nums {
 		nums[i] = rand.Intn(n * 10)
 	}
-	//fmt.Println("Before sort:", nums)
+	fmt.Println("生成完成")
 
 	// 快速排序
 	start1 := time.Now()
@@ -35,6 +35,23 @@ func main() {
 	fmt.Println("Median:", m.findMedian())
 	end2 := time.Now()
 	fmt.Println("线性时间选择耗时：", end2.Sub(start2))
+
+	// 随机选择
+	epochCnt := 0
+	start3 := time.Now()
+	if n%2 == 0 {
+		rs1 := randomSel{nums: nums, k: n / 2, epoch: 1}
+		rs2 := randomSel{nums: nums, k: n/2 + 1, epoch: 1}
+		fmt.Println("Median:", float64(rs1.sort()+rs2.sort())/2)
+		epochCnt = rs1.epoch + rs2.epoch
+	} else {
+		rs := randomSel{nums: nums, k: (n + 1) / 2, epoch: 1}
+		fmt.Println("Median:", float64(rs.sort()))
+		epochCnt = rs.epoch
+	}
+	end3 := time.Now()
+	fmt.Println("随机选择耗时：", end3.Sub(start3))
+	fmt.Println("随机选择递归次数：", epochCnt)
 
 	//fmt.Println("After sort:", nums)
 }
--- Lab/Lab2/code/random_select.go
+++ Lab/Lab2/code/random_select.go
@@ -1,1 +1,91 @@
 package main
+
+import (
+	"math"
+	"math/rand"
+	"sort"
+	"time"
+)
+
+func rank(x int, nums []int) int {
+	rank := 0
+	for _, num := range nums {
+		if num < x {
+			rank++
+		}
+	}
+	return rank
+}
+
+type randomSel struct {
+	nums  []int
+	k     int // 第 k 小的元素
+	epoch int // 递归次数
+}
+
+func (rs *randomSel) selectRandomElements(numElements int) []int {
+
+	// 使用当前时间作为随机种子
+	rand.Seed(time.Now().UnixNano())
+
+	// 创建一个映射来跟踪已经选择的索引
+	selectedIndices := make(map[int]bool)
+	selectedElements := make([]int, numElements)
+
+	// 从数组中选择指定数量的元素
+	for i := 0; i < numElements; {
+		// 生成随机索引
+		randomIndex := rand.Intn(len(rs.nums))
+
+		// 如果索引未被选择，则将其添加到已选择的索引列表中
+		if !selectedIndices[randomIndex] {
+			selectedIndices[randomIndex] = true
+			selectedElements[i] = rs.nums[randomIndex]
+			i++
+		}
+	}
+
+	return selectedElements
+}
+
+func (rs *randomSel) sort() int {
+	n := len(rs.nums)
+	selNums := rs.selectRandomElements(int(math.Pow(float64(n), 0.75)))
+
+	// 快速排序
+	sort.Ints(selNums)
+
+	// 理论上的kth数在排序后的数组中的索引
+	x := float64(rs.k) * math.Pow(float64(n), -0.25)
+
+	// 考察上下边界
+	left := math.Max(math.Floor(x-math.Pow(float64(n), 0.5)), 0)
+	right := math.Min(math.Floor(x+math.Pow(float64(n), 0.5)), float64(len(selNums)-1))
+
+	// 在有序数组中找到第l,h小的元素
+	L := selNums[int(left)]
+	H := selNums[int(right)]
+
+	// 计算rank
+	Lp := rank(L, rs.nums)
+	Hp := rank(H, rs.nums)
+
+	// 收集原数组中介于L和H之间的元素
+	var P []int
+	for _, num := range rs.nums {
+		if num >= L && num <= H {
+			P = append(P, num)
+		}
+
+	}
+
+	// 判断
+	if Lp <= rs.k && rs.k <= Hp && float64(len(P)) <= 4*(math.Pow(float64(n), 0.75))+1 {
+		sort.Ints(P)
+		return P[rs.k-Lp-1] // 返回第k - Lp 小的元素，注意索引偏移
+	} else {
+		rs.epoch++
+		return rs.sort()
+	}
+
+}
